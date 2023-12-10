8a307f87bf1bcba87329b815992dbe36fc47531a
janbako <jbako893@gmail.com>
2023-12-11 00:35:28 +0100

    changes

---
+++ src/Home/Home.js
@@ -0,0 +1,13 @@
+// src/Home/HomePage.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h2>Welcome to the Homepage</h2>
+      {/* Add your homepage content here */}
+    </div>
+  );
+};
+
+export default HomePage;
